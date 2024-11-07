9b42194ed732849421e44076849cdca57c1cbe37
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-11-07 15:40:58 -0300

    feat: add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Desafio
+
+Um professor de ensino médio solicitou aos seus alunos que desenvolvessem um programa para converter o valor do ponto de ebulição da água em Kelvin para Celsius.
+
+Dica: C = K - 273
--- kelvin-to-celsius.go
+++ kelvin-to-celsius.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	const valorDiferenca = 273.15
-	temperaturaKelvin := 100.0
+	temperaturaKelvin := 373.15
 	temperaturaCelsius := temperaturaKelvin - valorDiferenca
 
 	fmt.Printf(" %.2f°K equivale a %.2f°C\n", temperaturaKelvin, temperaturaCelsius)
